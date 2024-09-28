916443d723d77415c8efcdf0660461627552310f
Mli <mliotard@yahoo.fr>
2024-09-29 00:25:59 +0200

    concatMap

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { concat, of } from 'rxjs';
 
 
 @Component({
@@ -17,8 +18,12 @@ export class AboutComponent implements OnInit {
     // setTimeout(() => sub.unsubscribe(), 5000)
     // setTimeout(() => interval$.subscribe(val => console.log("start again" + val)), 5000)
      
-    
-    
+    const source1$ = of(1,2,3); // send 1,2,3 then complete
+    const source2$ = of(4,5,6);
+    const source3$ = of(7,8,9);
+    const result$ = concat(source1$, source2$, source3$);
+    result$.subscribe(console.log);
+    // same as result$.subscribe(val => console.log(val));
   }
 
 }
