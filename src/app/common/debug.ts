1718931561dde73fce46ea42d340d0436363a346
Mli <mliotard@yahoo.fr>
2024-10-03 23:27:10 +0200

    Debug operator.

---
+++ src/app/common/debug.ts
@@ -0,0 +1,26 @@
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+
+export enum RxJsLoggingLevel {
+    TRACE,
+    DEBUG,
+    INFO,
+    ERROR
+}
+
+let rxJsLoggingLevel = RxJsLoggingLevel.INFO;
+
+export function setRxJsLoggingLevel(level: RxJsLoggingLevel) {
+    rxJsLoggingLevel = level;
+}
+
+export const debug = (level: number, message: string) => 
+(source: Observable<any>) => source.
+pipe(
+    tap(val => {
+        if (level >= rxJsLoggingLevel) {
+            console.log(message + ':', val);
+        }
+        
+    })
+)
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -16,6 +16,7 @@ import {
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
+import { debug, RxJsLoggingLevel } from '../common/debug';
 
 
 @Component({
@@ -38,7 +39,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
     ngOnInit() {
     this.courseId = this.route.snapshot.params['id'];
         console.log(this.courseId);
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(debug(RxJsLoggingLevel.INFO, "course values"));
     }
 
     ngAfterViewInit() {
@@ -51,7 +52,11 @@ export class CourseComponent implements OnInit, AfterViewInit {
         this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
         .pipe(map(event => event.target.value), 
         startWith(''),
-        debounceTime(400), distinctUntilChanged(), switchMap(search => this.loadLessons(search)));
+        debug(RxJsLoggingLevel.INFO, "search"),
+        debounceTime(400),
+        distinctUntilChanged(),
+        switchMap(search => this.loadLessons(search)),
+        debug(RxJsLoggingLevel.INFO, "lessons value"));
 
     }
 
