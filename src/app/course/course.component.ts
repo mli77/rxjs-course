f39322857e69bc0befb8327e68a9e21028888808
Mli <mliotard@yahoo.fr>
2024-10-11 23:43:05 +0200

    first et take() operators.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -11,9 +11,10 @@ import {
     concatMap,
     switchMap,
     withLatestFrom,
-    concatAll, shareReplay
+    concatAll, shareReplay,
+    first
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {merge, fromEvent, Observable, concat, forkJoin} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import {createHttpObservable} from '../common/util';
 import {Store} from '../common/store.service';
@@ -45,6 +46,19 @@ export class CourseComponent implements OnInit, AfterViewInit {
         this.courseId = this.route.snapshot.params['id'];
 
         this.course$ = this.store.selectCourseById(this.courseId);
+
+    }
+
+    ngOnInitFirst() {
+
+        this.courseId = this.route.snapshot.params['id'];
+
+        this.course$ = this.store.selectCourseById(this.courseId)
+        .pipe(
+            first()
+        );
+
+        forkJoin(this.course$, this.loadLessons()).subscribe(console.log);
 
     }
 
